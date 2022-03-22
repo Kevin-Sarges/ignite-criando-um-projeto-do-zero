e39b67fdeb38ba8958d47a717eb79ac7d57e1a92
Kevin-Sarges <kevinsarges08@gmail.com>
2022-03-22 15:49:59 -0300

    Finalizando html e css

--- src/@types/png.d.ts
+++ src/@types/png.d.ts
@@ -1,1 +1,11 @@
-declare module '*.png';
+type StaticImageData = {
+  src: string;
+  height: number;
+  width: number;
+  placeholder?: string;
+};
+
+declare module '*.png' {
+  const content: StaticImageData;
+  export default content;
+}
--- src/pages/post/index.tsx
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default function Post() {
-  return <h1>Posts</h1>;
-}
