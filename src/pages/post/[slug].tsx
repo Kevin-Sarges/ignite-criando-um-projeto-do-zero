300a536c23f5e9dd58ab4b022aa21dac65317d81
Kevin-Sarges <kevinsarges08@gmail.com>
2022-03-28 21:03:11 -0300

    Ajustes

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import Image from 'next/image';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai';
 import { BiTimeFive } from 'react-icons/bi';
-import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { asHTML } from '@prismicio/helpers';
 
 import { getPrismicClient } from '../../services/prismic';
-
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
@@ -35,162 +37,41 @@ interface PostProps {
 export default function Post({ post }: PostProps) {
   return (
     <main className={styles.componente}>
-      <h1>Criando um app CRA do zero</h1>
-
-      <div className={styles.descriptionPost}>
-        <data className={commonStyles.infoPost}>
-          <AiOutlineCalendar />
-          <p>15 Mar 2021</p>
-        </data>
-
-        <div className={commonStyles.infoPost}>
-          <AiOutlineUser />
-          <p>Joseph Oliveira</p>
-        </div>
-
-        <div className={commonStyles.infoPost}>
-          <BiTimeFive />
-          <p>4 min</p>
-        </div>
+      <div className={styles.imagePost}>
+        <Image
+          src={post.data.banner.url}
+          alt="ImagePost"
+          width="1350px"
+          height="450px"
+          objectFit="cover"
+        />
       </div>
 
       <div className={styles.contentPost}>
-        <h2>Proin et varius</h2>
+        <h1>{post.data.title}</h1>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div className={styles.descriptionPost}>
+          <data className={commonStyles.infoPost}>
+            <AiOutlineCalendar />
+            <p>{post.first_publication_date}</p>
+          </data>
 
-        <br />
+          <div className={commonStyles.infoPost}>
+            <AiOutlineUser />
+            <p>{post.data.author}</p>
+          </div>
 
-        <p>
-          Nullam dolor sapien, vulputate eu diam at, condimentum hendrerit
-          tellus. Nam facilisis sodales felis, pharetra pharetra lectus auctor
-          sed.
-        </p>
+          <div className={commonStyles.infoPost}>
+            <BiTimeFive />
+            <p>4 min</p>
+          </div>
+        </div>
 
-        <br />
-
-        <p>
-          Ut venenatis mauris vel libero pretium, et pretium ligula faucibus.
-          Morbi nibh felis, elementum a posuere et, vulputate et erat. Nam
-          venenatis.
-        </p>
-
-        <br />
-
-        <h2>Cras laoreet mi</h2>
-
-        <p>
-          Nulla auctor sit amet quam vitae commodo. Sed risus justo, vulputate
-          quis neque eget, dictum sodales sem. In eget felis finibus, mattis
-          magna a, efficitur ex. Curabitur vitae justo consequat sapien gravida
-          auctor a non risus. Sed malesuada mauris nec orci congue, interdum
-          efficitur urna dignissim. Vivamus cursus elit sem, vel facilisis nulla
-          pretium consectetur. Nunc congue.
-        </p>
-
-        <br />
-
-        <p>
-          Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-          per inceptos himenaeos. Aliquam consectetur massa nec metus
-          condimentum, sed tincidunt enim tincidunt. Vestibulum fringilla risus
-          sit amet massa suscipit eleifend. Duis eget metus cursus, suscipit
-          ante ac, iaculis est. Donec accumsan enim sit amet lorem placerat, eu
-          dapibus ex porta. Etiam a est in leo pulvinar auctor. Praesent sed
-          vestibulum elit, consectetur egestas libero.
-        </p>
-
-        <br />
-
-        <p>
-          Ut varius quis velit sed cursus. Nunc libero ante, hendrerit eget
-          consectetur vel, viverra quis lectus. Sed vulputate id quam nec
-          tristique. Etiam lorem purus, imperdiet et porta in, placerat non
-          turpis. Cras pharetra nibh eu libero ullamcorper, at convallis orci
-          egestas. Fusce ut est tellus. Donec ac consectetur magna, nec
-          facilisis enim. Sed vel tortor consectetur, facilisis felis non,
-          accumsan risus. Integer vel nibh et turpis.
-        </p>
-
-        <br />
-
-        <p>
-          Nam eu sollicitudin neque, vel blandit dui. Aliquam luctus aliquet
-          ligula, sed:
-        </p>
-
-        <br />
-
-        <p>
-          Suspendisse ac facilisis leo. Sed nulla odio, aliquam ut lobortis
-          vitae, viverra quis risus. Vivamus pulvinar enim sit amet elit
-          porttitor bibendum. Nulla facilisi. Aliquam libero libero, porta ac
-          justo vitae, dapibus convallis sapien. Praesent a nibh pretium,
-          ultrices urna eget, vulputate felis. Phasellus ac sagittis ipsum, a
-          congue lectus. Integer interdum ut velit vehicula volutpat. Nulla
-          facilisi. Nulla rhoncus metus lorem, sit amet facilisis ipsum faucibus
-          et. Lorem ipsum.
-        </p>
-
-        <br />
-
-        <p>
-          Curabitur a rutrum ante. Praesent in justo sagittis, dignissim quam
-          facilisis, faucibus dolor. Vivamus sapien diam, faucibus sed sodales
-          sed, tincidunt quis sem. Donec tempus ipsum massa, ut fermentum ante
-          molestie consectetur. In hac habitasse platea dictumst. Sed non
-          finibus nibh, vitae dapibus arcu. Sed lorem magna, imperdiet non
-          pellentesque et, rhoncus ac enim. Class aptent taciti sociosqu ad
-          litora torquent per conubia.
-        </p>
-
-        <br />
-
-        <p>
-          Praesent ac sapien eros. Suspendisse potenti. Morbi eu ante nibh.
-          Proin dictum, tellus ut molestie tincidunt, urna tortor sodales velit,
-          ut tempor lectus ipsum nec sapien. Nulla nec purus vitae libero
-          aliquet posuere non et sapien. Cras in erat rhoncus, dignissim ligula
-          iaculis, faucibus orci. Donec ligula neque, imperdiet vitae mauris
-          eget, egestas varius massa. Praesent ornare nisi at dui dapibus, ac
-          tristique felis.
-        </p>
-
-        <br />
-
-        <p>
-          Phasellus maximus urna lacus, non imperdiet ex blandit sit amet.
-          Vivamus et tellus est. Mauris ligula elit, placerat non tellus a,
-          dictum porttitor urna. Phasellus mollis turpis id suscipit dapibus. In
-          dolor.
-        </p>
-
-        <br />
-
-        <p>
-          Sed sit amet euismod sapien, non eleifend erat. Vivamus et quam odio.
-          Integer nisi lacus, maximus sit amet turpis in, luctus molestie sem.
-          Duis sit amet euismod erat. Fusce pulvinar ex neque, egestas cursus
-          nulla ullamcorper vel. Pellentesque mollis erat egestas est rhoncus,
-          sit amet sodales massa ullamcorper. Etiam auctor ante a neque
-          facilisis tristique. Proin ultricies fringilla turpis, eget tempus
-          elit imperdiet non. Quisque.
-        </p>
-
-        <br />
-
-        <p>
-          Etiam eu tortor placerat, varius orci non, ornare nunc. Cras suscipit
-          in ligula ultricies lacinia. Pellentesque at tristique sapien, et
-          scelerisque leo. Donec eu nisi at magna tristique luctus vel at
-          turpis. Nam vestibulum ornare ex cursus vulputate. In elementum tellus
-          at sapien bibendum, id maximus mauris convallis. Donec facilisis porta
-          lobortis. Vivamus mauris diam, pretium ac dolor.
-        </p>
-
-        <br />
-
-        <p>Pellentesque et consequat arcu, ac laoreet ante. Nam non.</p>
+        {/* <div
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: post.data.content }}
+        className={styles.contentPost}
+      /> */}
       </div>
     </main>
   );
@@ -219,24 +100,26 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const response = await prismic.getByUID('posts', String(slug), {});
 
   const post = {
-    first_publication_date: response.first_publication_date,
+    first_publication_date: format(
+      new Date(response.first_publication_date),
+      'dd MMM yyyy',
+      { locale: ptBR }
+    ),
     data: {
       title: response.data.title,
       banner: {
-        url: response.url,
+        url: response.data.banner.url,
       },
       author: response.data.author,
-      content: response.data.content,
+      content: asHTML(response.data.content),
     },
   };
 
-  console.log(JSON.stringify(post, null, 2));
+  // console.log(JSON.stringify(post, null, 2));
 
   return {
     props: {
       post,
     },
-
-    revalidate: 60 * 30,
   };
 };
